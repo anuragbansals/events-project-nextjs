9a2417896af0771c32b5f1f9e3e026624497d14f
anurag-bansals <99685503+anurag-bansals@users.noreply.github.com>
2023-02-09 17:59:21 +0530

    filter WIP

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,35 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/eventDetail/eventSummary";
+import EventsLogistics from "../../components/eventDetail/eventLogistics";
+import EventContent from "../../components/eventDetail/eventContent";
+
 function EventDetailPage() {
-  return <div></div>;
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <div>
+      <Fragment>
+        <EventSummary title={event.title} />
+        <EventsLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </Fragment>
+    </div>
+  );
 }
 
 export default EventDetailPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,23 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/eventList";
+import EventsSearch from "../../components/events/eventsSearch";
+import { useRouter } from "next/router";
+
 function AllEventsPage() {
+  const events = getAllEvents();
+  const router = useRouter()
+  
+  function findEventsHandler(year, month){
+      const fullPath = `/events/${year}/${month}/abc`;
+
+    router.push();
+  }
+
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
     </div>
   );
 }
